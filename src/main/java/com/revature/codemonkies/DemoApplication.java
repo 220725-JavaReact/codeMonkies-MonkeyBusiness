a117e476c723817accdabeadd4d891595aa149a9
Brandon Learned <90928439+blearned92@users.noreply.github.com>
2022-09-07 14:44:50 -0600

    retry push

---
+++ src/main/java/com/revature/codemonkies/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.revature.codemonkies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
