021f63c88ea1b0cb9d3f10c23c0e494e1418bc78
Trenton <dr.teasdale.trenton@gmail.com>
2022-09-12 13:25:42 -0600

    deleted

--- Front-end/monkey-business/src/components/Menu.tsx
+++
@@ -1,9 +0,0 @@
-function Menu(){
-    return <div className="center">
-    <button>Blackjack</button>
-    <button>Coming Soon</button>
-    <button>Coming Soon</button>
-    <button>Coming Soon</button>
-    </div>
-}
-export default Menu;
