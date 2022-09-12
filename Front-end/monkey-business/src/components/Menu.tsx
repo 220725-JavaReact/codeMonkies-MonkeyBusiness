472a1275c73ebf17a39632d333570affa7ffd5e7
Trenton <dr.teasdale.trenton@gmail.com>
2022-09-12 13:21:35 -0600

    tsx react

---
+++ Front-end/monkey-business/src/components/Menu.tsx
@@ -0,0 +1,9 @@
+function Menu(){
+    return <div className="center">
+    <button>Blackjack</button>
+    <button>Coming Soon</button>
+    <button>Coming Soon</button>
+    <button>Coming Soon</button>
+    </div>
+}
+export default Menu;
