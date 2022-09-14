88fc31cdcf5b9af35e711a1a09bc3322551b6377
Brandon Learned <90928439+blearned92@users.noreply.github.com>
2022-09-14 12:09:08 -0600

    Login working without verification

--- Front-end/monkey-business/src/components/Menu.tsx
+++ Front-end/monkey-business/src/components/Menu.tsx
@@ -1,11 +1,22 @@
-function Menu(){
+import { UserLog } from "../models/UserLog";
+
+interface Props {
+    game:string;
+    setGame:React.Dispatch<React.SetStateAction<string>>;
+}
+
+interface ICombined extends Props, UserLog{
+}
+
+function Menu({isUserLoggedIn, setLog, game, setGame}:ICombined){
     return <div className="center">
         <div className="menu">
-    <button className="menuBtn">Blackjack</button>
-    <button className="menuBtn">Coming Soon</button>
-    <button className="menuBtn">Coming Soon</button>
-    <button className="menuBtn">Coming Soon</button>
+            <button className="menuBtn" onClick={(e)=>setGame("Blackjack")}>Blackjack</button>
+            <button className="menuBtn">Coming Soon</button>
+            <button className="menuBtn">Coming Soon</button>
+            <button className="menuBtn">Coming Soon</button>
         </div>
+        <button onClick={(e)=>setLog(false)}>Log Out</button>
     </div>
 }
 export default Menu;
